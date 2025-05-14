fd1ee016632fa4950fd034f385ee056effd639f1
TinyLoon <ilhanyildirim@hotmail.de>
2025-05-14 14:54:55 +0200

    update

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -1,7 +1,8 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('RENDER_EXTERNAL_URL', ''), // optional aber hilfreich
   app: {
-    keys: env.array('APP_KEYS') || ["tobemodified1", "tobemodified2"],
+    keys: env.array('APP_KEYS') || ['tobemodified1', 'tobemodified2'],
   },
 });
